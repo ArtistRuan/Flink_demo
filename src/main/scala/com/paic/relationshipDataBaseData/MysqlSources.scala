a3d234525c8d6f9d3c1accafec74d9681fa25412
ArtistRuan <m2ruan@126.com>
2021-06-10 22:34:47 +0800

    new

--- src/main/scala/com/paic/relationshipDataBaseData/MysqlSources.scala
+++ src/main/scala/com/paic/relationshipDataBaseData/MysqlSources.scala
@@ -4,6 +4,7 @@ import org.apache.flink.api.common.typeinfo.BasicTypeInfo
 import org.apache.flink.api.java.io.jdbc.JDBCInputFormat
 import org.apache.flink.api.java.typeutils.RowTypeInfo
 import org.apache.flink.api.scala._
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.types.Row
 
 /**
@@ -18,11 +19,11 @@ object MysqlSources {
   def main(args: Array[String]): Unit = {
     val env = ExecutionEnvironment.getExecutionEnvironment
 
-    jdbcRead(env)
-
+//    jdbcReadMysql(env)
+    jdbcReadPostgreSql(env)
   }
 //利用有返回值的方式，可以用获取到的数据根据业务进一步操作
-  def jdbcRead(env:ExecutionEnvironment):Unit={
+  def jdbcReadMysql(env:ExecutionEnvironment):Unit={
     val inputMysql:DataSet[Row] = env.createInput(
       JDBCInputFormat.buildJDBCInputFormat()
     //指定驱动名称
@@ -37,6 +38,30 @@ object MysqlSources {
     )
     inputMysql.print()
 //    env.execute()
+  }
+  //有问题，待改进
+  def jdbcReadPostgreSql(env:ExecutionEnvironment): Unit ={
+    val inputPG = env.createInput(
+      JDBCInputFormat.buildJDBCInputFormat()
+        .setDrivername("com.postgresql.jdbc.Driver")
+        .setDBUrl("jdbc:postgresql://localhost:5432/postgres")
+        .setUsername("postgres")
+        .setPassword("Paic1234")
+        .setQuery("select id,name,age,address,salary from company")
+        .setRowTypeInfo(
+          new RowTypeInfo(
+            BasicTypeInfo.INT_TYPE_INFO,
+            BasicTypeInfo.STRING_TYPE_INFO,
+            BasicTypeInfo.INT_TYPE_INFO,
+            BasicTypeInfo.STRING_TYPE_INFO,
+            BasicTypeInfo.BIG_INT_TYPE_INFO
+          )
+        )
+        .finish()
+    )
+
+    inputPG.print()
+//    inputPG.print("PG")
   }
 }
 
--- src/main/scala/com/paic/relationshipDataBaseData/PostgreSqlSource.scala
+++ src/main/scala/com/paic/relationshipDataBaseData/PostgreSqlSource.scala
@@ -1,5 +1,12 @@
 package com.paic.relationshipDataBaseData
 
+import org.apache.flink.connector.jdbc.catalog.JdbcCatalog
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.table.api._
+import org.apache.flink.table.api.scala._
+import org.apache.flink.types.Row
+
+
 /**
   *
   * @program: FlinkEngine
@@ -10,6 +17,36 @@ package com.paic.relationshipDataBaseData
   **/
 object PostgreSqlSource {
   def main(args: Array[String]): Unit = {
-    
+//    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+//    env.setParallelism(1)
+
+//    val tableEnv: StreamTableEnvironment = StreamTableEnvironment.create(env)
+
+    val settings = EnvironmentSettings.newInstance().inStreamingMode().build()
+    val tableEnv: TableEnvironment = TableEnvironment.create(settings)
+
+    val name            = "mypg"
+    val defaultDatabase = "postgres"
+    val username        = "postgres"
+    val password        = "Paic1234"
+    val baseUrl         = "jdbc:postgresql://localhost:5432/"
+
+    val catalog: JdbcCatalog = new JdbcCatalog(name, defaultDatabase, username, password, baseUrl)
+    tableEnv.registerCatalog("mypg", catalog)
+
+    // set the JdbcCatalog as the current catalog of the session
+    tableEnv.useCatalog("mypg")
+
+    val resultPg = tableEnv.sqlQuery(
+      """
+        |select * from `public.company`
+      """.stripMargin)
+
+    resultPg.toAppendStream[Row].print()
+
+//    env.execute()
   }
 }
+
+
+
